40d399b9bc44e5d44af914931d842fc8eec7930e
fadyehabamer <fadyamer45@gmail.com>
2021-04-21 07:37:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ window.addEventListener("load",()=>{
 async function registersw(){
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', function() {
-          navigator.serviceWorker.register('./sw.js').then(function(registration) {
+          navigator.serviceWorker.register('sw.js').then(function(registration) {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
           }, function(err) {
